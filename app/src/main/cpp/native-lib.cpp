db8c4c45c4cb002d65cee6192d6aeb7aca3f0847
Jay Tu <arden.jay@gmail.com>
2018-06-22 14:53:44 +0800

    Sets the i2c slave address from UI

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,9 +1,15 @@
 #include <jni.h>
 #include <string>
-#include<android/log.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <android/log.h>
 
 #define TAG "imx-i2c-jni"
 #define LOGD(...)  __android_log_print(ANDROID_LOG_DEBUG, TAG, __VA_ARGS__)
+
+#define I2C_SLAVE_FORCE 0x0706
+#define I2C_MASTER_DEV "/dev/i2c-0"
 
 extern "C" JNIEXPORT jstring
 JNICALL
@@ -19,8 +25,26 @@ Java_com_myandroid_imx_1i2c_MainActivity_stringFromJNI(
  * Method:    i2c_init
  * Signature: (I)V
  */
-extern "C" void JNICALL
+extern "C" int JNICALL
 Java_com_myandroid_imx_1i2c_MainActivity_i2c_1init
         (JNIEnv *env, jobject, jint addr) {
     LOGD("addr: 0x%x", addr);
+    int fd = open(I2C_MASTER_DEV, O_RDWR);
+    if (fd < 0) {
+        LOGD("open device error\n");
+        return -1;
+    }
+
+    int ret = ioctl(fd, I2C_SLAVE_FORCE, addr);
+    if (ret < 0) {
+        LOGD("Set slave address error (%d)\n", ret);
+        goto fail;
+    }
+
+    LOGD("successful\n");
+    return 0;
+
+fail:
+    close(fd);
+    return -1;
 }
